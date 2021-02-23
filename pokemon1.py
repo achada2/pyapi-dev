9c33294606d7461ba31d139a3e54b15d8f8c554c
Aditya Chada <aditya.chada@gmail.com>
2021-02-23 02:04:54 +0000

    poke

--- pokemon1.py
+++ pokemon1.py
@@ -1,9 +1,26 @@
+
 import requests
-poke_api="https://pokeapi.co/api/v2/pokemon/bulbasaur/"
+import wget
+#poke_api="https://pokeapi.co/api/v2/pokemon/bulbasaur/"
+
+def api_pull():
+    choice = input("What Pokemon picture would you like?")
+    url= "https://pokeapi.co/api/v2/pokemon/" + choice + "/"
+    return url
 
 def json_conv(poke_api):
     json2python = requests.get("poke_api").json()
     return json2python
-print(json_conv(poke_api))
-#poke_api=https://pokeapi.co/api/v2/pokemon/bulbasaur/)
+
+
+def api_slice(json2python):
+    poke_pic = json2python["sprites"]["front_default"]
+    return poke_pic
+
+def wget_pic(imagelink):
+    wget.download(imagelink, '/home/student/static/')
+
+def main():
+    wget_pic(api_slice(json_conv(api_pull())))
+main()
 
